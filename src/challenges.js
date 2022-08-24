eb9bafb09ed6a37ca10277b6e0c239bb6d1c3a2e
Pedro <pedrohalmeidamendonca@gmail.com>
2022-08-24 16:52:56 -0300

    Criando uma função que calcula a quantidade de pontos em um campeonato de futebol

--- src/challenges.js
+++ src/challenges.js
@@ -26,8 +26,9 @@ function concatName(words) {
 }
 
 // Desafio 5
-function footballPoints() {
+function footballPoints(wins, ties) {
   // seu código aqui
+  return wins * 3 + ties;
 }
 
 // Desafio 6
