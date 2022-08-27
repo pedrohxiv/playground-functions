6e5db582769bb7d47de89637b755927f5a3fcb54
Pedro <pedrohalmeidamendonca@gmail.com>
2022-08-27 20:53:39 -0300

    Resolvendo Requisitos Bônus

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,16 +1,71 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(arrayNumbers) {
   // seu código aqui
+  if (arrayNumbers.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index = 0; index < arrayNumbers.length; index += 1) {
+    if (arrayNumbers[index] < 0 || arrayNumbers[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let count = 0;
+  let element = 0;
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    count = 0;
+    element = arrayNumbers[i];
+    for (let j = 0; j < arrayNumbers.length; j += 1) {
+      if (element === arrayNumbers[j]) {
+        count += 1;
+      }
+    }
+    if (count >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let numbers =
+    '(' +
+    arrayNumbers[0] +
+    arrayNumbers[1] +
+    ') ' +
+    arrayNumbers[2] +
+    arrayNumbers[3] +
+    arrayNumbers[4] +
+    arrayNumbers[5] +
+    arrayNumbers[6] +
+    '-' +
+    arrayNumbers[7] +
+    arrayNumbers[8] +
+    arrayNumbers[9] +
+    arrayNumbers[10];
+  return numbers;
 }
 
 // Desafio 12
-function triangleCheck() {
+function triangleCheck(lineA, lineB, lineC) {
   // seu código aqui
+  if (lineA < lineB + lineC) {
+    if (lineB < lineA + lineC) {
+      if (lineC < lineA + lineB) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 // Desafio 13
-function hydrate() {
+function hydrate(drink) {
   // seu código aqui
+  let results = drink.match(/\d+/g);
+  let sumNumber = 0;
+  for (let index = 0; index < results.length; index += 1) {
+    sumNumber += parseInt(results[index]);
+  }
+  if (sumNumber === 1) {
+    return sumNumber + ' copo de água';
+  }
+  return sumNumber + ' copos de água';
 }
 
 module.exports = {
